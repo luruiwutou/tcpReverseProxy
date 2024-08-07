b3c4f05a5db3b64972a06e1db503c76725c2fddd
luruiwutou <luruiwutou@gmail.com>
2024-08-07 09:44:23 +0800

    添加文件名安全性校验

--- src/main/java/com/forward/core/tcpReverseProxy/utils/SecureFileAccess.java
+++ src/main/java/com/forward/core/tcpReverseProxy/utils/SecureFileAccess.java
@@ -11,14 +11,19 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class SecureFileAccess {
 
     public static String SAFE_DIR = ESAPI.securityConfiguration().getStringProp("Safe.Dir");
+    public static List<String> AllowedExtensions = Arrays.asList(ESAPI.securityConfiguration().getStringProp("AllowedExtensions").split(","));
+
     public static String getSafePath(String filePath) throws Exception {
         String path = filePath.substring(0, filePath.lastIndexOf(File.separator));
         String fileName = filePath.substring(filePath.lastIndexOf(File.separator) + 1);
+        ESAPI.validator().isValidFileName("Invalid file name ", fileName, AllowedExtensions,false);
         return ESAPI.validator().getValidDirectoryPath("Invalid file path ", path, new File(SAFE_DIR), false) + File.separator + fileName;
     }
 }
